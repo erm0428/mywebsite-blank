519024d1954f923fa8a62e56903b536ebf0d67e4
eoanalitica <123706289+eoanalitica@users.noreply.github.com>
2023-04-24 17:04:18 +0000

    Se agregó la vista de casa

--- project/portfolio_app/views.py
+++ project/portfolio_app/views.py
@@ -5,7 +5,11 @@ import numpy as np
 # Cargar modelo guardado
 filename = 'static/classification_svc_sklearn_1_2_2.pickle'
 with open(filename, 'rb') as f:
-    loaded_model = pickle.load(f)
+    loaded_model_classification = pickle.load(f)
+
+filename = 'static/regression_rf_sklearn_1_2_2.pickle'
+with open(filename, 'rb') as f:
+    loaded_model_regression = pickle.load(f)
 
 # Vista Index
 def index(request):
@@ -27,7 +31,7 @@ def iris(request):
 
         lista_iris = [slength, swidth, plength, pwidth]
         x_new = np.array([lista_iris])
-        y_pred = int(loaded_model.predict(x_new))
+        y_pred = int(loaded_model_classification.predict(x_new))
 
         dictionary = {0:('Iris Setosa', 'iris-setosa.png'), 
                       1:('Iris Versicolor', 'iris-versicolor.png'), 
@@ -39,5 +43,31 @@ def iris(request):
     template = 'portfolio_app/iris.html'
     context = {'pred_label': pred_label, 
                'pred_image_name': pred_image_name}
+    
+    return render(request, template, context)
+
+
+# Vista Casa
+def casa(request):
+
+    pred_label = ""
+
+    if request.method == "POST":
+
+        overall_quality = int(request.POST.get('OverallQual'))
+        above_ground_living_area = int(request.POST.get('GrLivArea'))
+        first_floor_sf = int(request.POST.get('1stFlrSF'))
+        full_bath = int(request.POST.get('FullBath'))
+        year_built = int(request.POST.get('YearBuilt'))
+
+        lista_casa = [overall_quality, above_ground_living_area, first_floor_sf, full_bath, year_built]
+
+        x_new = np.array([lista_casa])
+        y_pred = float(loaded_model_regression.predict(x_new))
+        
+        pred_label = f"{y_pred:.2f}"
+
+    template = 'portfolio_app/casa.html'
+    context = {'pred_label': pred_label}
     
     return render(request, template, context)
--- project/project/settings.py
+++ project/project/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = "django-insecure-rhoc9l&1ek3o)wnapn1e4e4l@9e00s-njr0!(iq=%y^a^5d^&c
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://mywebsite-blank-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://mywebsite-blank-production.up.railway.app',
+                        'https://eoanalitica-effective-goggles-r9vvwggg9q7f5gr-8000.preview.app.github.dev']
 
 # Application definition
 
